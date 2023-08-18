fd6219e83b386a731b04b7c94686404a226d6770
pedroMontandon <pedrobarbosa89@gmail.com>
2023-08-18 15:06:49 -0300

    study_schedule done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
-def study_schedule(permanence_period, target_time):
-    
+def study_schedule(permanence_period, target_time=None):
+    students = 0
+    for st in permanence_period:
+        try:
+            if (st[0] <= target_time and st[1] >= target_time):
+                students += 1
+        except (TypeError, ValueError):
+            return None
+    return students
