dc331ab1815c608770d13715c4b647091ad9e706
pedroMontandon <pedrobarbosa89@gmail.com>
2023-08-18 20:23:30 -0300

    find the duplicate done

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,19 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    
+    
+    # word1 = ''.join(sorted(first_string.lower()))
+    # word2 = ''.join(sorted(second_string.lower()))
+    # if len(first_string) != len(second_string):
+    #     return (word1, word2, False)
+    
+    # for index in range(len(word1)):
+    #     if first_string[index] != second_string[len(first_string) - 1 - index]:
+    #         return (word1, word2, False)
+    # return (word1, word2, True)
+
+print(is_anagram('roma', 'amor'))
+print(is_anagram('rio', 'ryo'))
+print(is_anagram('pedra', 'perdaaa'))
+
+# tem que ser O(n log n)
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,15 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        numbers = sorted(nums)
+        past_number = None
+        for number in numbers:
+            if past_number == number and number > 0:
+                return past_number
+            past_number = number
+        return False
+    except TypeError:
+        return False
+   
+
+print(find_duplicate([3, 1, 3, 4, 2]))
+print(find_duplicate([1, 3, 4, 2, 2]))
