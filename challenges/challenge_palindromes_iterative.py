76d2f28a76d3e8724287d559f8db65a3a0bf0879
pedroMontandon <pedrobarbosa89@gmail.com>
2023-08-18 15:58:08 -0300

    iterative palindromes done

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (len(word) < 1):
+        return False
+    for index in range(len(word) // 2):
+        if word[index] != word[len(word) - 1 - index]:
+            return False
+    return True
+
+
+print(is_palindrome_iterative(''))
+print(is_palindrome_iterative('SOCOS'))
+print(is_palindrome_iterative('REVIVER'))
+print(is_palindrome_iterative('COXINHA'))
+print(is_palindrome_iterative('AGUA'))
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,16 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        if word[low_index] != word[high_index]:
+            return False
+        if len(word) / 2 >= high_index:
+            return True
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    except IndexError:
+        return False
+
+
+print(is_palindrome_recursive('ANA', 0, len('ANA') - 1))
+print(is_palindrome_recursive('SOCOS', 0, len('SOCOS') - 1))
+print(is_palindrome_recursive('REVIVER', 0, len('REVIVER') - 1))
+print(is_palindrome_recursive('COXINHA', 0, len('COXINHA') - 1))
+print(is_palindrome_recursive('AGUA', 0, len('AGUA') - 1))
